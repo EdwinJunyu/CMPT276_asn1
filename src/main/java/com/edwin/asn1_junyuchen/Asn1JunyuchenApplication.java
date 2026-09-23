2b111a97f4f468ff3f53a139ce5b18a536762a67
Edwin_Chen <junyuchen0907@gmail.com>
2026-09-22 22:57:13 -0700

    Initial project setup

---
+++ src/main/java/com/edwin/asn1_junyuchen/Asn1JunyuchenApplication.java
@@ -0,0 +1,13 @@
+package com.edwin.asn1_junyuchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asn1JunyuchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Asn1JunyuchenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edwin/asn1_junyuchen/Asn1JunyuchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwin.asn1_junyuchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asn1JunyuchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
